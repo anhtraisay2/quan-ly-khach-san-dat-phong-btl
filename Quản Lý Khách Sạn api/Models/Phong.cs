eb00813f10db3647a13594fc27b2361275a0fb38
anhtraisay2 <[200~>
2025-11-02 18:00:08 +0700

    Thêm Model: Phong — thông tin phòng khách sạn

---
+++ Quản Lý Khách Sạn api/Models/Phong.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreApi.Models
+{
+    public class Phong
+    {
+        [Key]
+        public int MaPhong { get; set; }
+
+        [Required, MaxLength(20)]
+        public string SoPhong { get; set; }
+
+        [Required]
+        public int MaLoaiPhong { get; set; }
+        [ForeignKey("MaLoaiPhong")]
+        public virtual LoaiPhong? LoaiPhong { get; set; }  // thêm "virtual" và cho phép null để tránh vòng lặp JSON
+
+        [Required, MaxLength(50)]
+        public string TinhTrang { get; set; } = "SanSang";
+    }
+}
